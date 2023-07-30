29831bd2b59c920864ff3fad2a0025a7103759a2
NickoTesla <mykolandwirt@gmail.com>
2023-07-30 10:28:50 -0600

    update

--- created_app/app_new/models.py
+++ created_app/app_new/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    about = models.TextField()
+
+class Quote(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
--- created_app/app_new/views.py
+++ created_app/app_new/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Author
+from .models import Quote
+
 
 # Create your views here.
 def index(request):
-    return render(request, "app_new/index.html")
+    return render(request, "app_new/index.html", context={"author.name": "Main Page"})
+
+def add_author(request):
+    # код для додавання нового автора
+    pass
+
+@login_required
+def add_quote(request):
+    # код для додавання нової цитати
+    pass
+
+def author_detail(request, author_id):
+    author = Author.objects.get(pk=author_id)
+    return render(request, 'author_detail.html', {'author': author})
+
+def all_quotes(request):
+    quotes = Quote.objects.all()
+    return render(request, 'all_quotes.html', {'quotes': quotes})
